06487982c881cd0354b1e5b307340bf6c39a8795
Ewerton <ewerton.pluri@gmail.com>
2021-02-27 14:35:17 -0300

    feature: home component created

---
+++ src/app/components/home/home.module.ts
@@ -0,0 +1,11 @@
+import { HomeComponent } from './home.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [ CommonModule ],
+  exports: [HomeComponent],
+  providers: [],
+})
+export class HomeModule {}
