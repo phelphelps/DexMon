c5a35bc2de443ae078cad2e310f453df1fa0532b
Ewerton <ewerton.pluri@gmail.com>
2021-02-27 14:36:04 -0300

    refactor: common constants defined

--- src/app/shared/constants/general-constants.ts
+++ src/app/shared/constants/general-constants.ts
@@ -1,3 +1,4 @@
-export const DEFAULT_BASE_URL = 'https://pokeapi.co/api/v2/ability/';
+export const API_BASE_URL = 'https://pokeapi.co/api/v2/ability/';
+export const IMG_BASE_URL = 'https://pokeres.bastionbot.org/images/pokemon';
 
 export const APPLICATION_NAME = 'DexMon';
